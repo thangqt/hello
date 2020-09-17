38b62753a35cfdc102260c2699273aa3d6343cd7
thangqt <63841132+thangqt@users.noreply.github.com>
2020-09-17 11:57:48 +0700

    ádasdasđa
    ádasd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -a -q)
 docker pull thangqt/app:latest
 docker run -d --name webapp -p 8080:8080 -d thangqt/app 
